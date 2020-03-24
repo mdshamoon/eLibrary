bbf0a67f0c4d16fb3fa118b0d0da67f1b08cd783
mdshamoon <mohdshamoon841@gmail.com>
2020-03-24 11:28:48 +0530

    email verification completed

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('login/google', 'Auth\LoginController@redirectToProvider');
 Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
