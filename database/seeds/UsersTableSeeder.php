8e74ea6a8a3b168439e547bd9296b4788a7ea1c7
mdshamoon <mohdshamoon841@gmail.com>
2020-04-23 14:36:38 +0530

    some mistakes

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use App\User;
+use Carbon\Carbon;
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -20,10 +22,12 @@ class UsersTableSeeder extends Seeder
 
         $admin = User::create(['name' => "Admin",
                                 'email'=> "admin@admin.com",
+                                'email_verified_at'=> Carbon::now(),
                                 'password'=> Hash::make("12345678") ]);
          
         $user = User::create(['name' => "Shamoon",
                                 'email'=> "shamoonmalik143@gmail.com",
+                                'email_verified_at'=> Carbon::now(),
                                 'password'=> Hash::make("12345678") ]);   
                                 
         $admin->assignRole('admin');
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -152,7 +152,7 @@
 
 
   
-  <link href="{{asset('vendor/datatables/datatables.bootstrap4.min.css')}}" rel="stylesheet">
+  <link href="{{asset('vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
 <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
