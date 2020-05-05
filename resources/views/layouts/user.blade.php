884510bf20784f2a40b527164b56e14b54984dfc
mdshamoon <mohdshamoon841@gmail.com>
2020-05-05 17:20:05 +0530

    button removed

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -43,9 +43,7 @@
               
 
           <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
+        
 
         
 
